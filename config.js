d366b213761476c320bcd43abf1c5bc86a7a67cd
gubstav <guso22ab@student.cbs.dk>
2023-02-22 10:53:36 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+        host: "mysql108.unoeuro.com",
+        user: "supersede_dk",
+        password: "r25xeGhpyRAE6FBD4dwc",
+        database: "supersede_dk_db_programmering",
+    },
+};
+
+module.exports = config;
