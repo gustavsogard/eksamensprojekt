114de6f195892d31faf5c06b4be2793361d31bc5
thel22ab <thoeger1000@gmail.com>
2023-04-18 14:29:48 +0200

    Article EJS fil

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -2,5 +2,5 @@ const Articles = require('../models/Articles.js');
 
 exports.renderArticle = async (req,res) => {
     const article = await Articles('getById', {id: req.params.id})
-    res.render('../views/pages/article.ejs')
+    res.render('../views/pages/article.ejs', {article: article})
 }
