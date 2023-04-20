d90de8e98ef4673904c5eb64c1c86e2f682d9e81
TroelsPR <trows.rohde@gmail.com>
2023-04-20 10:33:24 +0200

    Added weather function to db

---
+++ cronjob/cron-weather.js
@@ -0,0 +1,37 @@
+var CronJob = require('cron').CronJob;
+const Weather = require('../models/Weather')
+
+var job = new CronJob(
+    '*/30 * * * * *',
+    function() {
+            const getWeather = async () => fetch(`https://api.open-meteo.com/v1/forecast?latitude=55.68&longitude=12.52&daily=temperature_2m_max,temperature_2m_min&past_days=31&timezone=Europe/Copenhagen`)
+            .then((response) => response.json())           
+            .then((data => {
+                for (let i = 0; i < 30; i++) {
+                    const weatherObject_historical = {
+                        city: data.timezone,
+                        date: data.daily.time[i],
+                        degrees: data.daily.temperature_2m_max
+                      }
+                    Weather('historical', weatherObject_historical)
+                }
+                for (let i = 30; i < 38; i++) {
+                    const weatherObject_forecast = {
+                        city: data.timezone,
+                        date: data.daily.time[i],
+                        degrees: data.daily.temperature_2m_max
+                      }
+                    Weather('forecast', weatherObject_forecast)
+                }
+                
+            }))
+        getWeather()
+        console.log('DONE');
+            
+        }
+      
+    ,
+    null,
+    true
+);
+job.start()
---
+++ models/Weather.js
@@ -0,0 +1,84 @@
+// Import the necessary modules
+const { Connection, Request, TYPES } = require('tedious');
+const config = require('../config');
+
+// Define the Users class and its methods
+function Weather(operation, obj) {
+    const connection = new Connection(config);
+
+    // Return a Promise for asynchronous handling
+    return new Promise((resolve, reject) => {
+        // Set up a database connection
+        connection.on('connect', (err) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+            } else {
+                let query = '';
+                let parameters = {};
+
+                // Set up the SQL query and parameters based on the specified operation
+                switch (operation) {
+                    case 'historical':
+                        query = 'INSERT INTO weater_historical (city, date, degrees) VALUES (@city, @date, @degrees)';
+                        parameters = {
+                            city: TYPES.VarChar,
+                            date: TYPES.DateTime,
+                            degrees: TYPES.Int
+                        };
+                        break;
+                    case 'forecast':
+                        query = 'INSERT INTO weater_forecast (city, date, degrees) VALUES (@city, @date, @degrees)';
+                        parameters = {
+                            city: TYPES.VarChar,
+                            date: TYPES.DateTime,
+                            degrees: TYPES.Int
+                        };
+                        break;
+                    default:
+                        console.log('No operation specified');
+                        reject(new Error('No operation specified'));
+                }
+
+                // Create a new Request object with the SQL query and parameters
+                const request = new Request(query, (err) => {
+                    if (err) {
+                        console.log(err);
+                        reject(err);
+                    } else {
+                        connection.close();
+                    }
+                });
+
+                // Add the parameters to the Request object
+                Object.keys(parameters).forEach((name) => {
+                    request.addParameter(name, parameters[name], obj[name]);
+                });
+
+                let response = undefined;
+
+                // Handle each row of the response
+                request.on('row', (columns) => {
+                    response = {};
+                    columns.forEach((column) => {
+                        response[column.metadata.colName] = column.value;
+                    });
+                });
+
+                // Handle the completion of the request
+                request.on('requestCompleted', () => {
+                    resolve(response);
+                });
+
+                // Execute the SQL query
+                connection.execSql(request);
+            }
+        });
+
+        // Connect to the database
+        connection.connect();
+    });
+}
+
+// Export the Users class
+module.exports = Weather;
