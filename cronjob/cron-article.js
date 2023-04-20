cf3cc382a8effec0fc9fb0ab5f6620d20ae3087c
thel22ab <thoeger1000@gmail.com>
2023-04-20 09:12:17 +0200

    Tilføjede category_id
    Det virker ikke pågrund af foreign key constraint

--- cronjob/cron-article.js
+++ cronjob/cron-article.js
@@ -2,6 +2,14 @@ var CronJob = require('cron').CronJob;
 const { Connection, Request, TYPES } = require('tedious');
 const config = require('../config');
 
+// der er en foreign key constraint, der gør at vi ikke kan lægge kategorierne ind
+/* 
+           triggerUncaughtException(err, true /* fromPromise );
+           ^
+
+           RequestError: The INSERT statement conflicted with the FOREIGN KEY constraint "FK__articles__catego__0E391C95". The conflict occurred in database "CBS", table "dbo.categories", column 'id'.
+*/
+
 
 // definerer max karakterer, for vores database
 const MAX_DESCRIPTION_LENGTH = 255;
@@ -19,7 +27,7 @@ function Articles(obj) {
                 reject(err);
             } else {
                 // Set up the SQL query and parameters based on the specified operation
-                query = 'INSERT INTO articles (title, description, source, author, url, image, published_at) VALUES (@title, @description, @source, @author, @url, @image, @published_At)';
+                query = 'INSERT INTO articles (title, description, source, author, url, image, published_at, category_id ) VALUES (@title, @description, @source, @author, @url, @image, @published_At, @category)';
                 parameters = {
                     title: TYPES.VarChar,
                     description: TYPES.VarChar,
@@ -27,7 +35,8 @@ function Articles(obj) {
                     author: TYPES.VarChar,
                     url: TYPES.VarChar,
                     image: TYPES.VarChar, 
-                    published_at: TYPES.VarChar
+                    published_at: TYPES.VarChar,
+                    category: TYPES.Int
                 };
                         
                 
@@ -73,43 +82,49 @@ function Articles(obj) {
     });
 }
 
-let categories = ['sport', 'technology', 'environment']
+const categories = ['business', 'entertainment', 'general']
 
 var job = new CronJob(
-    '*/59 * * * * *',
+    '*/30 * * * * *',
     function() {
-       const downloadBatch = async () => fetch(`https://newsapi.org/v2/top-headlines?category=${categories[i]}&apiKey=5608686a49a04c6e8db72943b518feb5`)
-        .then((response) => response.json())           
-        .then((data => {
-            for (let i = 0; i < 10; i++) {
-                    let description = data.articles[i].description;
-                    // tjekker om description eksisterer, altså den ikke er null
-                    if (description) {
-                        // hvis den eksisterer, tjekker om den er længere end 255, som er vores max
-                        if (description.length > MAX_DESCRIPTION_LENGTH) {
-                            // skærer description ned til at passe til vores maks
-                            description = description.slice(0, MAX_DESCRIPTION_LENGTH);
-                        } 
-                        // hvis den ikke eksisterer bliver den blot sat til null. 
-                    } else {
-                        description = null;
-                    }
-
-                const dataKeys = {
-                    title: data.articles[i].title,
-                    description: description,
-                    author: data.articles[i].author,
-                    source: data.articles[i].source.name,
-                    url: data.articles[i].url,
-                    image: data.articles[i].urlToImage,
-                    published_at: data.articles[i].publishedAt // 
-                  }
-                Articles(dataKeys)
-            }
+        for (let j = 0; j < categories.length; j++) {
+            console.log(categories[j]);
+            const downloadBatch = async () => fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${categories[j]}&apiKey=5608686a49a04c6e8db72943b518feb5`)
+            .then((response) => response.json())           
+            .then((data => {
+                for (let i = 0; i < 2; i++) {
+                        let description = data.articles[i].description;
+                        // tjekker om description eksisterer, altså den ikke er null
+                        if (description) {
+                            // hvis den eksisterer, tjekker om den er længere end 255, som er vores max
+                            if (description.length > MAX_DESCRIPTION_LENGTH) {
+                                // skærer description ned til at passe til vores maks
+                                description = description.slice(0, MAX_DESCRIPTION_LENGTH);
+                            } 
+                            // hvis den ikke eksisterer bliver den blot sat til null. 
+                        } else {
+                            description = null;
+                        }
+    
+                    const dataKeys = {
+                        title: data.articles[i].title,
+                        description: description,
+                        author: data.articles[i].author,
+                        source: data.articles[i].source.name,
+                        url: data.articles[i].url,
+                        image: data.articles[i].urlToImage,
+                        published_at: data.articles[i].publishedAt,
+                        category: j// 
+                      }
+                    Articles(dataKeys)
+                }
+                
+            }))
+        downloadBatch()
+        console.log('DONE');
             
-        }))
-    downloadBatch()
-    console.log('DONE');
+        }
+      
     }
     ,
     null,
