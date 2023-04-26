f678828aef49dcaba6a4df9d83a36db0a72d3410
TroelsPR <trows.rohde@gmail.com>
2023-04-26 14:08:49 +0200

    Tilføjer weathercode til vejret

--- cronjob/cron-weather.js
+++ cronjob/cron-weather.js
@@ -3,35 +3,33 @@ const Weather = require('../models/Weather')
 
 var job = new CronJob(
     '*/15 * * * * *',
-    function() {
-        Weather('truncate_tables')
-            const getWeather = async () => fetch(`https://api.open-meteo.com/v1/forecast?latitude=55.68&longitude=12.52&daily=temperature_2m_max,temperature_2m_min&past_days=31&timezone=Europe%2FBerlin`)
-            .then((response) => response.json())           
-            .then((data => {
-                for (let i = 0; i < 31; i++) {
-                    const weatherObject_historical = {
-                        city: data.timezone,
-                        date: data.daily.time[i],
-                        degrees: data.daily.temperature_2m_max[i]
-                      }
-                      
-                    Weather('historical', weatherObject_historical)
-                }
-                for (let i = 31; i < 38; i++) {
-                    const weatherObject_forecast = {
-                        city: data.timezone,
-                        date: data.daily.time[i],
-                        degrees: data.daily.temperature_2m_max[i]
-                      }
-                    Weather('forecast', weatherObject_forecast)
-                }
-                
-            }))
-        getWeather()
-        console.log('DONE');
-            
+    async function() {
+        Weather('truncate_tables');
+        await fetch(`https://api.open-meteo.com/v1/forecast?latitude=55.68&longitude=12.52&daily=weathercode,temperature_2m_max,temperature_2m_min&past_days=31&timezone=Europe%2FBerlin`)
+        .then((response) => response.json())           
+        .then((data => {
+            console.log("running data")
+            for (let i = 0; i < 31; i++) {
+                const weatherObject_historical = {
+                    city: data.timezone,
+                    date: data.daily.time[i],
+                    degrees: data.daily.temperature_2m_max[i],
+                    weathercode: data.daily.weathercode[i]
+                    }
+                Weather('historical', weatherObject_historical)
+            }
+            for (let i = 31; i < 38; i++) {
+                const weatherObject_forecast = {
+                    city: data.timezone,
+                    date: data.daily.time[i],
+                    degrees: data.daily.temperature_2m_max[i],
+                    weathercode: data.daily.weathercode[i]
+                    }
+                Weather('forecast', weatherObject_forecast)
+            }
+        }))
+        console.log('DONE');   
         }
-      
     ,
     null,
     true
