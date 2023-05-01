c32c830481cc649f25b520edd4d712770b92ab9b
thel22ab <thoeger1000@gmail.com>
2023-05-01 11:28:26 +0200

    Update cron-weather.js

--- cronjob/cron-weather.js
+++ cronjob/cron-weather.js
@@ -8,7 +8,7 @@ var job = new CronJob(
         await fetch(`https://api.open-meteo.com/v1/forecast?latitude=55.68&longitude=12.52&daily=weathercode,sunrise,sunset,temperature_2m_max,temperature_2m_min&past_days=31&timezone=Europe%2FBerlin`)
         .then((response) => response.json())
         .then((data => {
-            console.log("running data")
+            console.log("STARTING CRONJOB")
             for (let i = 0; i < 31; i++) {
                 const weatherObject_historical = {
                     city: data.timezone,
