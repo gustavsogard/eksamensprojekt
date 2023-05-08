e296746e98054065c4b251522cb50015d85cc9e9
gubstav <guso22ab@student.cbs.dk>
2023-05-08 22:38:58 +0200

    Implemented load more on search

--- controllers/search.controllers.js
+++ controllers/search.controllers.js
@@ -1,6 +1,11 @@
 const Search = require('../models/Search.js');
 
 exports.renderSearch = async (req, res) => {
-    const articles = await Search('search', {keyWord: req.body.query, publisher: req.body.publisher, page: 0})
-    res.render('../views/pages/search.ejs', {articles: articles});
+    const articles = await Search('search', {keyWord: req.body.query, publisher: req.body.publisher, page: 0});
+    res.render('../views/pages/search.ejs', {articles: articles, query: req.body.query, publisher: req.body.publisher});
+}
+
+exports.loadMoreSearch = async (req, res) => {
+    const articles = await Search('search', {keyWord: req.params.query, publisher: req.params.publisher, page: req.params.page});
+    res.json(articles);
 }
--- routes/search.route.js
+++ routes/search.route.js
@@ -5,7 +5,11 @@ const searchController = require('../controllers/search.controllers')
 router.route('/search')
     .post((req,res) => {
         searchController.renderSearch(req,res)
+    })
 
+router.route('/search/loadmore/:page/:query/:publisher')
+    .get((req, res) => {
+        searchController.loadMoreSearch(req, res);
     })
 
 module.exports = router;
