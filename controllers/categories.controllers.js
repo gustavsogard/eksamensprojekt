567aec3d1b606ab79f6dd62b098694bf6f4b1ddb
gubstav <guso22ab@student.cbs.dk>
2023-05-10 09:03:15 +0200

    Update categories.controllers.js

--- controllers/categories.controllers.js
+++ controllers/categories.controllers.js
@@ -10,8 +10,6 @@ exports.renderCategory = async (req, res) => {
         user_id = req.session.user.id;
     }
 
-    // hvis id ikke er et tal stopper funktionen
-
     // article bliver defineret som de 12 artikler der bliver returneret fra Articles modellen, når operationen 'get12ByCategoryId' bliver sendt.
     // Der bliver også sendt id med, fra params, for at søge på artikel id'et. 
     const articles = await Articles('get12ByCategoryId', {category_id: req.params.id, page: 0});
